61eb3a8bc5df21886886ab9d1799aebc3471f3fb
Sankhasubhra Mandal <33262790+Sankha98@users.noreply.github.com>
2020-04-23 04:10:23 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr1[n];
+    int arr2[n];
+    for(int i = 0; i < n;i++)
+    {
+        cin >> arr1[i];
+    }
+    for(int i = 0; i < n;i++)
+    {
+        cin >> arr2[i];
+    }
+    int arr[n];
+    for(int i = 0;i < n;i++)
+    {
+        arr[i] = arr1[i] + arr2[i];
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
